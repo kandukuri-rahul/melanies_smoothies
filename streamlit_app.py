f1a32f8c3f909c58452b584d66dbb4c2cb399f16
kandukuri-rahul <rahul.kandukuri@kasmodigital.com>
2025-08-20 11:11:50 +0530

    Update streamlit_app.py
    added into for loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
-# Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f" :cup_with_straw: Customize Your Smoothies :cup_with_straw:")
@@ -34,7 +33,9 @@ if ingredients_list:
     ingredients_string=''
     for fruits_choosen in ingredients_list:
         ingredients_string+=fruits_choosen+" "
-    #st.write(ingredients_string)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+  
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     
@@ -46,9 +47,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
     st.stop()
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
+
 
 
